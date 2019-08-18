67908f8e1b3f6c5f50bf5aacde69fd6f6607f35e
Dalton <Lima>
2019-08-18 16:09:29 +0200

    we know have an emulated piano on the keyboard

--- SoundSynthesizer/Main.cpp
+++ SoundSynthesizer/Main.cpp
@@ -3,14 +3,26 @@
 
 #include "olcNoiseMaker.h"
 
+atomic<double> dFrequencyOutput = 0.0;
 
 // Function used by olcNoiseMaker to generate sound waves
 // Returns amplitude (-1.0 to +1.0) as a function of time
 double MakeNoise(double dTime)
 {
-	// Sine functions doesn't take Hz as parameters, but Angular velocities, thus we (2 * Pi)
-	// Playing A 440Hz
-	return 0.5 * sin(440.0 * (2 * PI) * dTime);
+	// Sine functions doesn't take Hz as parameters, but Angular velocities, thus we multiply by it by (2 * Pi
+	double dOutput = 1.0 * sin(dFrequencyOutput * (2 * PI) * dTime);// +sin((dFrequencyOutput + 20.0) * (2 * PI) * dTime);;
+
+	//return dOutput * 0.4;
+
+	// emulating a Square wave
+	if (dOutput > 0.0)
+	{
+		return 0.2;
+	}
+	else
+	{
+		return -0.2;
+	}
 }
 
 int main()
@@ -32,11 +44,26 @@ int main()
 
 	// Link noise function with sound machine
 	sound.SetUserFunction(MakeNoise);
-	
 
+	const double dOctaveBaseFrequency = 110.0; // A2
+	const double d12thRootOf2 = pow(2.0, 1.0 / 12.0);
+	
 	while (true)
 	{
-		
+		// Add a keyboard like a piano
+		bool bKeyPressed = false;
+		for (int k = 0; k < 15; k++)
+		{
+			if (GetAsyncKeyState(static_cast<unsigned char>("ZSXCFVVGBNJMK\xbcL\xbe"[k])) & 0x8000)
+			{
+				dFrequencyOutput = dOctaveBaseFrequency * pow(d12thRootOf2, k);
+				bKeyPressed = true;
+			}
+		}
+		if (!bKeyPressed)
+		{
+			dFrequencyOutput = 0.0f;
+		}
 	}
 	
 	return 0;
